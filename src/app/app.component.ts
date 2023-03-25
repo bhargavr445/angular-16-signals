6b562bfc4c3c79614d4cfb85d732e5d1563d9423
Bhargav Guntaka <bhargavr445@gmail.com>
2023-03-25 11:04:19 -0400

    upd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { single } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-15-signals';
+
+  newTitle = single('');
 }
