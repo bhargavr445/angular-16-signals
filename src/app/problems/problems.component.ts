4d441c9f277683ac33f5d82775d54818ac452064
Bhargav Guntaka <bhargavr445@gmail.com>
2023-05-08 17:08:59 -0400

    api call and holding response into signals

--- src/app/problems/problems.component.ts
+++ src/app/problems/problems.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit, effect, signal } from '@angular/core';
 import { ProblemsDetailsComponent } from './problems-details/problems-details.component';
 import { Router, RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { NgFor } from '@angular/common';
+import { StudentService } from '../student.service';
+import { ResponseI } from './ressponseI';
 
-type eventType = 'Increment' | 'Decrement'
+type eventType = 'Increment' | 'Decrement';
+
 @Component({
   selector: 'app-problems',
   standalone: true,
   templateUrl: './problems.component.html',
   styleUrls: ['./problems.component.scss'],
-  imports: [ProblemsDetailsComponent, RouterModule, CommonModule]
+  imports: [ProblemsDetailsComponent, RouterModule, NgFor]
 })
 
 
@@ -17,10 +20,13 @@ export class ProblemsComponent implements OnInit {
 
   counter = signal(0);
   actions = signal<string[]>([]);
+  responseAction = signal<ResponseI>(this.initialResponse());
 
-  constructor(private router: Router) {
+  constructor(private router: Router,
+    private studentService: StudentService) {
 
   }
+
   ngOnInit(): void {
     effect(() => {
       this.counter();
@@ -30,19 +36,34 @@ export class ProblemsComponent implements OnInit {
     });
   }
 
-  counterClick(type: eventType) {
+  initialResponse(): ResponseI {
+    return { total_count: 0, incomplete_results: false, items: [] }
+  }
+
+  counterClick(type: eventType): void {
     if (type === 'Increment') {
       this.counter.set(1 + this.counter());
     } else {
       this.counter.set(this.counter() - 1);
     }
-    this.actions.update((prevValues) => [...prevValues, type]);
-    this.actions.mutate((prevValues) => [...prevValues, type]);
-
+    // this.actions.update((prevValues) => [...prevValues, type]);
+    this.actions.mutate((prevValues) => prevValues.push(type));
   }
 
-  navigateTo(path) {
+  navigateTo(path: string): void {
     this.router.navigate([path]);
+  }
+
+  getData(): void {
+    this.responseAction.set(this.initialResponse());
+    this.studentService.getInfo().subscribe({
+      next: (response: ResponseI) => {
+        this.responseAction.set(response);
+      },
+      error: (error) => {
+
+      }
+    })
   }
 
 }
--- src/app/student.service.ts
+++ src/app/student.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs';
+import { Observable, map } from 'rxjs';
+import { ResponseI } from './problems/ressponseI';
 
 @Injectable({
   providedIn: 'root'
@@ -9,8 +10,8 @@ export class StudentService {
 
   constructor(private http: HttpClient) { }
 
-  getInfo() {
-    return this.http.get('https://api.github.com/search/issues?q=repo:angular/components&sort=created&order=asc&page=2').pipe(
+  getInfo(): Observable<ResponseI> {
+    return this.http.get<ResponseI>('https://api.github.com/search/issues?q=repo:angular/components&sort=created&order=asc&page=2').pipe(
       map((response) => ({ ...response }))
     )
   }
